65c0fb524ff5553d9f3cf398c4ddb7acb7162912
sayid-khan <sayidkhan365@gmail.com>
2024-08-08 21:53:41 +0530

    added dynamic data and added route

--- app.py
+++ app.py
@@ -1,11 +1,44 @@
-from flask import Flask
+from flask import Flask, render_template, jsonify
 
 app = Flask(__name__) #we are creating a object of Flask(as app) [becoz flask is a class]
+
+JOBS = [
+    {
+        'id' : 1,
+        'title' : 'Data Analyst',
+        'location' : 'Bengaluru',
+        'salary' : 'Rs. 100000'
+    },
+    {
+        'id' : 2,
+        'title' : 'Data Scientist',
+        'location' : 'Bengaluru',
+        'salary' : 'Rs. 900000'
+    },
+    {
+        'id' : 3,
+        'title' : 'Frontend Engineer',
+        'location' : 'Remote',
+    },
+    {
+        'id' : 4,
+        'title' : 'Backend Engineer',
+        'location' : 'San Fransisco, USA',
+        'salary' : '$ 50000'
+    }
+
+]
 
 #decorator for creating route
 @app.route("/",methods=["GET"])
 def welcome():
-    return "Welcome to the channel"
+    return render_template('home.html', jobs= JOBS, company_name = 'Jovian')
+
+
+@app.route("/api/jobs")
+def list_jobs():
+    return jsonify(JOBS)
+
 
 if __name__ == "__main__" :
     app.run(debug=True)
